53337999e88267b31aad8b8a7283ec2b5ea3a5c0
Daksh Thakran <daksh.thakran05@gmail.com>
2025-03-09 13:46:40 +0530

    Update BooksBought.jsx

--- frontend/src/components/BooksBought.jsx
+++ frontend/src/components/BooksBought.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect } from "react";
 import { fetchTransactions } from "../store/useBookStore.js";
 import { useTranslation } from "react-i18next";
+import { Loader } from "lucide-react";
 
 const BooksBoughtList = ({ userId }) => {
   const { t } = useTranslation();
-  const { booksBought, fetchBooksBought } = fetchTransactions();
+  const { booksBought, fetchBooksBought, isLoadingBought } = fetchTransactions();
 
   useEffect(() => {
     fetchBooksBought(userId);
   }, [userId]);
 
   return (
-    <div className="p-4 mt-16">
+    <div className="p-4 mt-16 relative">
       <h2 className="text-xl font-semibold mb-4">{t("booksBoughtTitle")}</h2>
-      {booksBought.length === 0 ? (
+
+      {isLoadingBought ? (
+        <div className="flex items-center justify-center h-40">
+          <Loader className="animate-spin text-blue-500 w-10 h-10" />
+        </div>
+      ) : booksBought.length === 0 ? (
         <p>{t("booksBoughtNoBooks")}</p>
       ) : (
         <ol className="list-decimal pl-5">
